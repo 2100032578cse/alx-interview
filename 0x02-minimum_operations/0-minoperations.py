0fe6965dd0ee070d923358a644f698d100f13627
Sandrine <99196826+2100032578cse@users.noreply.github.com>
2024-07-08 21:35:23 +0530

    func to get few # of operations needed to result in n H chars

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+def minOperations(n):
+    """
+    func to get few # of operations needed to result in n H chars
+    """
+    if n < 2:
+        return 0
+    ops, root = 0, 2
+    while root <= n:
+        if n % root == 0:
+            ops += root
+            n = n / root
+            root -= 1
+        root += 1
+    return ops

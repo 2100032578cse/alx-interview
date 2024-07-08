340a988a5544062478ac1d6827d46207fe9a1d69
Sandrine <99196826+2100032578cse@users.noreply.github.com>
2024-07-08 21:48:26 +0530

    operations needed to result in n in H chars

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -5,11 +5,13 @@ def minOperations(n):
     """
     if n < 2:
         return 0
-    ops, root = 0, 2
-    while root <= n:
-        if n % root == 0:
-            ops += root
-            n = n / root
-            root -= 1
-        root += 1
-    return ops
+
+    total_ops = 0
+
+    # Iterate over possible divisors from 2 to n
+    for divisor in range(2, n + 1):
+        # Repeatedly divide n by the current divisor
+        while n % divisor == 0:
+            total_ops = total_ops + divisor
+            n //= divisor
+    return total_ops

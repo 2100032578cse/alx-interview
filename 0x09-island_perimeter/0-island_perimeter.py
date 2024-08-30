2b76b7c17ed2280a4e4e4a72a136f001b79d1a86
2100032578cse <uwizeyesandri@gmail.com>
2024-08-29 18:57:19 -0700

    island parameter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+problem for island perimeter
+"""
+
+
+def island_perimeter(grid):
+    """
+    calculatinf the island parameter
+    Args:
+        grid: 2d list, 0(water), 1(land)
+    return:
+        island parameter
+    """
+    pr=0
